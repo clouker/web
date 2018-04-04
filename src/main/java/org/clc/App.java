bf2855f34afbff86abef43adb0d5feaa56fc8f72
louzj <louzj@hzcominfo.com>
2018-04-04 09:22:58 +0800

    update

--- src/main/java/org/clc/App.java
+++ src/main/java/org/clc/App.java
@@ -2,10 +2,15 @@ package org.clc;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Connector;
 import org.clc.config.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatEmbeddedWebappClassLoader;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -38,10 +43,10 @@ public class App implements WebMvcConfigurer {
      * @param registry
      */
     public void addInterceptors(InterceptorRegistry registry) {
-        log.info("InterceptorRegistry : Start...");
-        log.info("          ---loginInterceptor : Open...");
+//        log.info("InterceptorRegistry : Start...");
+//        log.info("          ---loginInterceptor : Open...");
         registry.addInterceptor(loginInterceptor);
-        log.info("InterceptorRegistry : Success...");
+//        log.info("InterceptorRegistry : Success...");
     }
 
     /**
@@ -59,7 +64,6 @@ public class App implements WebMvcConfigurer {
 //                .apis(RequestHandlerSelectors.basePackage("org.clc.web.controller.*.*"))
                 .build();
     }
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("网站API文档")
@@ -69,5 +73,18 @@ public class App implements WebMvcConfigurer {
                 //.contact(new Contact("name", "url", "email"))
                 .build();
     }
+
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(0);
+//        return connector;
+//    }
 
 }
